0eda5e64809c476bcbc8ecabe302b372a1c5ad31
Stephen H <stephen210huang@hotmail.com>
2017-02-08 18:25:20 -0500

    reserve page created, index page complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  # get 'reservations/show'
+  # get 'reservations/create'
+  # get 'reservations/destroy'
+
+  resources :reservations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
