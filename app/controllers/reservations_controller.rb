5b371ea2f2ababf70ccf795ed408bb0d582c813b
Stephen H <stephen210huang@hotmail.com>
2017-02-09 15:06:54 -0500

    List of detail for reservation somewhat done

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,11 +8,34 @@ class ReservationsController < ApplicationController
     @reserve = Reservation.find(params[:id])
   end
 
-  def create
+  def new
+    # this is just to render the form to make a new reservation
+    @reserve = Reservation.new
+  end
 
+  def create
+    # this is for creating a new Reservation
+
+    reserve = Reservation.new(reserve_params)
+    # do we have @reserve.id here? NO
+    # in other words: @reserve.id != nil MEANS that it has been saved to the database
+
+    save_result = reserve.save
+
+    if save_result
+      redirect_to reservation_url(reserve.id)
+    else
+      # if it didn't save, what should we do?
+    end
   end
 
   def destroy
 
   end
+
+  private
+  def reserve_params
+    params.require(:reservation).permit(:date, :people, :restaurant, :hours)
+  end
+
 end
