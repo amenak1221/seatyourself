9402a50b4efe48417166c08ca1af656f942d5933
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-02-09 13:05:54 -0500

    added fake restaurants to db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,63 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.create(
+name: 'Food Bytes',
+address: '220 King Street West',
+cuisine: 'Pub Grub'
+)
+
+Restaurant.create(
+name: 'Big Wong'
+address: '123 Spadina ave'
+cuisine: 'Chinese'
+)
+
+Restaurant.create(
+name: 'Marios Pizza'
+address: '456 Collage Street'
+cuisine: 'Italian'
+)
+
+Restaurant.create(
+name: 'Fresho'
+address: '73 queen Street'
+cuisine: 'Vegetarian'
+)
+
+Restaurant.create(
+name: 'Happy Sushi'
+address: '90 John Street'
+cuisine: 'Sushi'
+)
+
+Restaurant.create(
+name: 'Gringo Mandingo'
+address: '69 Dundas Street'
+cuisine: 'Mexican'
+)
+
+Restaurant.create(
+name: 'Luigi Pizza'
+address: '457 Collage Street'
+cuisine: 'Italian'
+)
+
+Restaurant.create(
+name: 'McDonnells'
+address: '333 Yonge Street'
+cuisine: 'Junk Food'
+)
+
+Restaurant.create(
+name: 'KFT (Kentucky fried Turkey)'
+address: '12 Front Street'
+cuisine: 'Junk Food'
+)
+
+Restaurant.create(
+name: 'Pizza Pizza'
+address: '878 Sherbourne Street'
+cuisine: 'Pizza'
+)
